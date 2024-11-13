d8100a047e83280845120de078689e187f8f3b41
Lucas <lucasssfrancoo@gmail.com>
2024-11-12 21:03:46 -0300

    Reestructuracion de funciones

---
+++ graficos.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+def crear_grafico(pronostico_por_dia):
+    dias = list(pronostico_por_dia.keys())
+    temps_max = [max(d['temp'] for d in pronostico) for pronostico in pronostico_por_dia.values()]
+    plt.figure(figsize=(10, 5))
+    plt.plot(dias, temps_max, marker='o')
+    plt.title("Temperaturas Máximas por Día")
+    plt.xlabel("Días")
+    plt.ylabel("Temperatura (°C)")
+    plt.grid(visible=True)
+    plt.savefig("static/grafico_temperaturas.png")
+    plt.close()
